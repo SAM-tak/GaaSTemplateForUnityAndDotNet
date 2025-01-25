4e85aba14e9dc799fc0d63e6937dae2d11838085
SAM-tak <osamu.takasugi@gmail.com>
2025-01-26 06:01:38 +0900

    開発証明書エキスポートスクリプトを移動

--- export_dev_certs.ps1
+++
@@ -1,5 +0,0 @@
-Write-Output "Export Developer Certification to StreamingAssets Folder"
-$destloc = Get-ChildItem YourGameClient\Assets\StreamingAssets
-Push-Location YourGameServer
-dotnet dev-certs https -ep $destloc\ca.crt --format Pem
-Pop-Location
--- export_dev_certs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-echo Export Developer Certification to StreamingAssets Folder
-destloc=$(find *Client/Assets/StreamingAssets -print -quit)
-cd *Server
-dotnet dev-certs https -ep ../$destloc/ca.crt --format Pem
-cd ..
