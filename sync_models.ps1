c107a2b97001a116ebfcab6b13f8492ee314985a
SAM-tak <osamu.takasugi@gmail.com>
2025-01-26 11:57:34 +0900

    update

--- sync_models.ps1
+++
@@ -1,4 +0,0 @@
-Write-Output "Server To Client"
-robocopy .\YourGameServer\Models .\YourGameClient\Assets\YourGameClient\Scripts\ServerSchemes /xo
-Write-Output "Client To Server"
-robocopy .\YourGameClient\Assets\YourGameClient\Scripts\ServerSchemes .\YourGameServer\Models /xo /xf *.meta
--- sync_models.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-echo Server To Client
-rsync -auv ./YourGameServer/Models/ ./YourGameClient/Assets/YourGameClient/Scripts/ServerSchemes/
-echo Client To Server
-rsync -auv --exclude=*.meta ./YourGameClient/Assets/YourGameClient/Scripts/ServerSchemes/ ./YourGameServer/Models/
